8ab9d8df63c6261a219f591f79d29b675e2df4f8
Yu <yu@hiya.moe>
2024-09-25 06:19:48 -0600

    Removed redundant code

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1,43 +1,4 @@
-﻿//namespace Hangman
-//{
-//    partial class Form1
-//    {
-//        /// <summary>
-//        /// Required designer variable.
-//        /// </summary>
-//        private System.ComponentModel.IContainer components = null;
-
-//        /// <summary>
-//        /// Clean up any resources being used.
-//        /// </summary>
-//        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-//        protected override void Dispose(bool disposing)
-//        {
-//            if (disposing && (components != null))
-//            {
-//                components.Dispose();
-//            }
-//            base.Dispose(disposing);
-//        }
-
-//        #region Windows Form Designer generated code
-
-//        /// <summary>
-//        /// Required method for Designer support - do not modify
-//        /// the contents of this method with the code editor.
-//        /// </summary>
-//        private void InitializeComponent()
-//        {
-//            this.components = new System.ComponentModel.Container();
-//            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-//            this.ClientSize = new System.Drawing.Size(800, 450);
-//            this.Text = "Form1";
-//        }
-
-//        #endregion
-//    }
-//}
-using static System.Net.Mime.MediaTypeNames;
+﻿using static System.Net.Mime.MediaTypeNames;
 using System.Drawing;
 using System.Windows.Forms;
 using System.Xml.Linq;
@@ -713,4 +674,3 @@ namespace Hangman
         #endregion
     }
 }
-
