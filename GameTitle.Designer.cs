58fbd5bdf9d9746535a80080f02c6996a548355e
Yu <yu@hiya.moe>
2024-09-29 09:50:56 -0600

    Renamed picture name and team name

--- GameTitle.Designer.cs
+++ GameTitle.Designer.cs
@@ -28,13 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            
             this.lb_start_game = new System.Windows.Forms.Label();
             this.lb_copyright = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            pb_logo = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(pb_logo)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pb_hanging = new System.Windows.Forms.PictureBox();
+            this.pb_logo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_hanging)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_logo)).BeginInit();
             this.SuspendLayout();
             // 
             // lb_start_game
@@ -54,30 +53,31 @@
             this.lb_copyright.Name = "lb_copyright";
             this.lb_copyright.Size = new System.Drawing.Size(393, 24);
             this.lb_copyright.TabIndex = 3;
-            this.lb_copyright.Text = "© 2024 Summoners With Insomnia. All right reserved.";
+            this.lb_copyright.Text = "© 2024 Summoners with Insomnia. All right reserved.";
             this.lb_copyright.Click += new System.EventHandler(this.startGame);
+            // 
+            // pb_hanging
+            // 
+            this.pb_hanging.Image = global::Hangman.Properties.Resources.hanging;
+            this.pb_hanging.Location = new System.Drawing.Point(286, 27);
+            this.pb_hanging.Name = "pb_hanging";
+            this.pb_hanging.Size = new System.Drawing.Size(134, 184);
+            this.pb_hanging.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_hanging.TabIndex = 2;
+            this.pb_hanging.TabStop = false;
+            this.pb_hanging.Click += new System.EventHandler(this.startGame);
             // 
             // pb_logo
             // 
-            pb_logo.BackColor = System.Drawing.Color.Transparent;
-            pb_logo.Image = global::Hangman.Properties.Resources.logo;
-            pb_logo.Location = new System.Drawing.Point(136, 178);
-            pb_logo.Name = "pb_logo";
-            pb_logo.Size = new System.Drawing.Size(413, 125);
-            pb_logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            pb_logo.TabIndex = 1;
-            pb_logo.TabStop = false;
-            pb_logo.Click += new System.EventHandler(this.startGame);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::Hangman.Properties.Resources.hanging;
-            this.pictureBox1.Location = new System.Drawing.Point(286, 27);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(134, 184);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.pb_logo.BackColor = System.Drawing.Color.Transparent;
+            this.pb_logo.Image = global::Hangman.Properties.Resources.logo;
+            this.pb_logo.Location = new System.Drawing.Point(136, 178);
+            this.pb_logo.Name = "pb_logo";
+            this.pb_logo.Size = new System.Drawing.Size(413, 125);
+            this.pb_logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_logo.TabIndex = 1;
+            this.pb_logo.TabStop = false;
+            this.pb_logo.Click += new System.EventHandler(this.startGame);
             // 
             // GameTitle
             // 
@@ -85,7 +85,7 @@
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(692, 530);
             this.Controls.Add(pb_logo);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pb_hanging);
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(710, 577);
             this.MinimumSize = new System.Drawing.Size(710, 577);
@@ -93,10 +93,9 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Hangman";
             this.Click += new System.EventHandler(this.startGame);
-            ((System.ComponentModel.ISupportInitialize)(pb_logo)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_hanging)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_logo)).EndInit();
             this.ResumeLayout(false);
-
         }
 
         private System.Windows.Forms.Label lb_copyright;
@@ -104,6 +103,6 @@
         private System.Windows.Forms.PictureBox pb_logo;
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pb_hanging;
     }
 }
